07a9fd530e2708628702cc08a3c44f31eef5ab51
edgar193241146 <80978764+edgar193241146@users.noreply.github.com>
2021-03-26 09:04:39 -0600

    Update and rename js/CtrlSaludo.js to init.js

--- js/CtrlSaludo.js
+++
@@ -1,9 +0,0 @@
-export class CtrlSaludo{
-  calculaSaludo(nombre){
-    const nombreSinEspacios = nombre ? nombre.trim(): "";
-    if(!nombreSinEspacios){
-      throw new Error("Falta proporcionar el nombre.");
-    }
-    return `Hola ${nombreSinEspacios}.`;
-  }
-}
